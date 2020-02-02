1ce282eaa9577877657d8a492b2510501977a9dc
InkyinnHmwe <inkyinnh@gmail.com>
2020-02-02 11:08:22 +0630

    post delete

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -53,4 +53,38 @@ router.get('/:id', function(req,res,next){
     }
   })
 })
+
+router.patch('/:id', function(req, res){
+  var update = {};
+  for (var opt of req.body) {
+    update[opt.proName]=opt.proValue
+  }
+Post.findByIdAndUpdate(req.params.id, {$set:update},function(err, rtn){
+    if(err){
+      res.status(500).json({
+        message:"Internal server error",
+        error:err
+      })
+    }else {
+      res.status(201).json({
+        message:"User account updated"
+      })
+    }
+  })
+})
+
+router.delete('/:id', function(req,res){
+  Post.findByIdAndRemove(req.params.id,function(err, rtn){
+    if (err){
+      res.status(500).json({
+        message:"Internal server error",
+        error:err
+      })
+    }else {
+      res.status(200).json({
+        message:"Post deleted"
+      })
+    }
+  })
+})
 module.exports = router;
